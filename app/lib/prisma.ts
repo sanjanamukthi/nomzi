286ce6b8ebf78d709701b7dec997994b421fa54b
Sanjana Mukthi <sanjanamukthi@gmail.com>
2026-08-08 21:48:20 +0530

    Use standard Prisma client

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { PrismaLibSql } from "@prisma/adapter-libsql";
 
 const adapter = new PrismaLibSql({
